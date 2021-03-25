25692ee1cda257f20f0cb9fe2c1da5c6d2a51cd3
Suri <sunderbharath85@gmail.com>
2021-03-24 19:38:40 -0700

    expriment with same domain

--- pacakges/container/config/webpack.prod.js
+++ pacakges/container/config/webpack.prod.js
@@ -14,7 +14,7 @@ const prodConfig = {
     new ModuleFederationPlugin({
       name: "container",
       remotes: {
-        "marketing": `marketing@${domain}/marketing/remoteEntry.js`
+        "marketing": `marketing@/marketing/remoteEntry.js`
       },
       shared: packageJson.dependencies
     })
